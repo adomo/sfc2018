be5d99dc1643f8ef6d651333bf6960bb927a5100
Dara McGann <dara.mcgann@WRLT0259.local>
2018-08-28 20:25:48 +0100

    completed function I hope

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
-const functions = require('firebase-functions');
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as request from 'request-promise';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config.firebase);
+
+const topic = 'mass';
+
+export let manageTopics = functions.database
+    .ref('topics/{topic}/user/{user}')
+    .onWrite(async event => {
+        let action = event.data.exists() ? 'batchAdd' : 'batchRemove';
+
+        let tokenSnapshot = await admin.database()
+            .ref('pushIds/${event.params.user}')
+            .once('value');
+
+        if(!tokenSnapshot.exists()) {
+            console.log('User has no tokens!')
+            return null;
+        }
+
+        let pushTokens = Object.keys(tokenSnapshot.val());
+        console.log('Adding ${pushtokens.length()} tokens to ${topic}');
+
+        let oauthToken = await functions.config().firebase.credential.getAccesstoken();
+
+        await request({
+            url : 'https://iid.googleapis.com/iid/vs/${action}',
+            method : 'POST',
+            json : true,
+            headers : {
+                Authorization : 'Bearer ${oauthToken.access_token}',
+                access_token_auth : true,
+            },
+            body : {
+                to : 'topics/${topic}',
+                registration_tokens : pushTokens
+            }
+        })
+    });
